9be0417f47893adb50ffc1b29dd94f4f4e8c5626
Mikael Stockman <micke@stockman.se>
2023-07-11 15:10:21 +0200

    Solution with better performance for ticket traversal.

--- vilket-sl-kort/tickets.js
+++ vilket-sl-kort/tickets.js
@@ -1,47 +1,47 @@
 let singleTicket = {
-    "title": "Enkelbiljett 75 minuter",
-    "priceFull": 39,
-    "priceReduced": 26,
-    "days": null
+    title: "Enkelbiljett 75 minuter",
+    priceFull: 39,
+    priceReduced: 26,
+    days: 0
 }
 
 let dayTicket = {
-    "title": "24-timmarsbiljett",
-    "priceFull": 165,
-    "priceReduced": 110,
-    "days": 1
+    title: "24-timmarsbiljett",
+    priceFull: 165,
+    priceReduced: 110,
+    days: 1
 }
 
 let tickets = [
     {
-        "title": "Årsbiljett",
-        "priceFull": 10190,
-        "priceReduced": 6830,
-        "days": 365
+        title: "Årsbiljett",
+        priceFull: 10190,
+        priceReduced: 6830,
+        days: 365
     },
     {
-        "title": "90&#8209;dagarsbiljett",
-        "priceFull": 2810,
-        "priceReduced": 1880,
-        "days": 90
+        title: "90&#8209;dagarsbiljett",
+        priceFull: 2810,
+        priceReduced: 1880,
+        days: 90
     },
     {
-        "title": "30&#8209;dagarsbiljett",
-        "priceFull": 970,
-        "priceReduced": 650,
-        "days": 30
+        title: "30&#8209;dagarsbiljett",
+        priceFull: 970,
+        priceReduced: 650,
+        days: 30
     },
     {
-        "title": "7&#8209;dagarsbiljett",
-        "priceFull": 440,
-        "priceReduced": 290,
-        "days": 7
+        title: "7&#8209;dagarsbiljett",
+        priceFull: 440,
+        priceReduced: 290,
+        days: 7
     },
     {
-        "title": "72&#8209;timmarsbiljett",
-        "priceFull": 330,
-        "priceReduced": 220,
-        "days": 3
+        title: "72&#8209;timmarsbiljett",
+        priceFull: 330,
+        priceReduced: 220,
+        days: 3
     },
     dayTicket,
     singleTicket,
@@ -53,26 +53,45 @@ $(document).ready(function () {
     updateCalculations()
 })
 
+function getNumberOfDays() {
+    return parseInt($("#nrDaysInput").val())
+}
+
+function getTravelsPerDay() {
+    return parseInt($("#travelsPerDayInput").val())
+}
+
+function getTotalTravelsCount() {
+    return parseInt($("#totalTravelsCount").val())
+}
+
 $('#nrDaysInput').on('input', function () {
-    let nrOfDays = $("#nrDaysInput").val()
-    let travelsPerDay = $("#travelsPerDayInput").val()
-    $('#totalTravelsCount').prop('value', nrOfDays * travelsPerDay)
-    updateCalculations()
+    let nrOfDays = getNumberOfDays()
+    let travelsPerDay = getTravelsPerDay()
+    if (nrOfDays > 0 && travelsPerDay > 0) {
+        $('#totalTravelsCount').prop('value', nrOfDays * travelsPerDay)
+        updateCalculations()
+    }
 });
 
 $('#travelsPerDayInput').on('input', function () {
-    let nrOfDays = $("#nrDaysInput").val()
-    let travelsPerDay = $("#travelsPerDayInput").val()
-    $('#totalTravelsCount').prop('value', nrOfDays * travelsPerDay)
-    updateCalculations()
+    let nrOfDays = getNumberOfDays()
+    let travelsPerDay = getTravelsPerDay()
+    if (nrOfDays > 0 && travelsPerDay > 0) {
+        $('#totalTravelsCount').prop('value', nrOfDays * travelsPerDay)
+        updateCalculations()
+    }
 });
 
 $('#totalTravelsCount').on('input', function () {
-    let nrOfDays = $("#nrDaysInput").val()
-    let totalTravels = $("#totalTravelsCount").val()
-    let travelsPerDay = (totalTravels / nrOfDays).toFixed(1)
-    $('#travelsPerDayInput').prop('value', travelsPerDay)
-    updateCalculations()
+    let nrOfDays = getNumberOfDays()
+    let totalTravels = getTotalTravelsCount()
+
+    if (nrOfDays > 0 && totalTravels > 0) {
+        let travelsPerDay = (totalTravels / nrOfDays).toFixed(1)
+        $('#travelsPerDayInput').prop('value', travelsPerDay)
+        updateCalculations()
+    }
 });
 
 
@@ -110,7 +129,7 @@ function formatEdgeDescrition(edge, isFirstLine, hasMultipleLines) {
     }
 }
 
-function formatOptionDescrition(option) {
+function formatOptionDescription(option) {
     let cheapestOptionDescription = option.edges
         .map((edge, index) => formatEdgeDescrition(edge, index == 0, option.edges.length > 1))
         .join('')
@@ -118,21 +137,27 @@ function formatOptionDescrition(option) {
 }
 
 function updateCalculations() {
+    let nrOfDays = getNumberOfDays()
+    let totalTravelsCount = getTotalTravelsCount()
 
-    let nrOfDays = $("#nrDaysInput").val()
-    let totalTravelsCount = $("#totalTravelsCount").val()
+    if (nrOfDays > 0 && totalTravelsCount > 0) {
+        let allOptions = calculateBestOption(nrOfDays, totalTravelsCount)
+        displayOptions(allOptions)
+    }
+}
 
-    let allOptions = calculateBestOption(nrOfDays, totalTravelsCount)
+function displayOptions(options) {
+    options.sort((a, b) => a.totalCostFull - b.totalCostFull)
+    let cheapestOption = options.shift()
 
     $('#calculatorResultTable tbody tr').remove()
-    let cheapestOption = allOptions.shift()
-    $('#best-option-ticket-desciption').html(`${formatOptionDescrition(cheapestOption)}`)
+    $('#best-option-ticket-desciption').html(`${formatOptionDescription(cheapestOption)}`)
     $('#best-option-ticket-total-cost-full').html(formatPrice(cheapestOption.totalCostFull))
     $('#best-option-ticket-total-cost-reduced').html(formatPrice(cheapestOption.totalCostReduced))
 
-    allOptions.forEach(option => {
+    options.forEach(option => {
         let row = $('<tr>');
-        row.append($('<td class="optionDescription">').html(formatOptionDescrition(option)));
+        row.append($('<td class="optionDescription">').html(formatOptionDescription(option)));
         row.append($('<td class="sized-table-column">').text(formatPrice(option.totalCostFull)));
         row.append($('<td class="sized-table-column">').text(formatPrice(option.totalCostReduced)));
 
@@ -142,111 +167,108 @@ function updateCalculations() {
 
 function calculateBestOption(nrOfDays, totalTravelsCount) {
     let travelsPerDay = totalTravelsCount / nrOfDays
-
-    let allOptions = createOptionsForNode([], [...tickets], totalTravelsCount, travelsPerDay, nrOfDays)
-
-    // Filter out all stupid options
-    let priceIfOnlySingleTickets = nrOfDays * travelsPerDay * singleTicket.priceFull
     let dayTicketWorthIt = travelsPerDay * singleTicket.priceFull > dayTicket.priceFull
+    let filteredTickets = tickets.filter(ticket => ticket != dayTicket || dayTicketWorthIt)
 
-    let filteredOptions = allOptions
-        .filter(option => {
-            if (option.edges.length == 1) {
-                return true
-            }
+    let rootEdge = {
+        ticket: null,
+        count: 0,
+        days: 0,
+        remainingTickets: filteredTickets,
+    }
 
-            if (option.edges.some(edge => edge.ticket == dayTicket) && !dayTicketWorthIt) {
-                return false
-            }
-
-            if (option.edges.some(edge => edge.ticket == singleTicket) && dayTicketWorthIt) {
-                return false
-            }
-
-            if (option.totalCostFull > priceIfOnlySingleTickets) {
-                return false
-            }
-
-            return true
-        }
-        )
-    filteredOptions.sort((a, b) => a.totalCostFull - b.totalCostFull)
-    let cheapestOption = filteredOptions[0]
-
-    return filteredOptions.filter(option => {
-        if (option.edges.length == 1) {
-            return true
-        }
-        if (option.edges.length > 2 && option.edges.length > cheapestOption.edges.length) {
-            return false
-        }
-        if (option.totalCostFull > cheapestOption.totalCostFull * 1.5) {
-            return false
-        }
-        return true
-    })
+    return createOptionsForNode([rootEdge], totalTravelsCount, travelsPerDay, nrOfDays)
 }
 
-function createOptionsForNode(traversedEdges, remainingTicketTypes, totalTravelsCount, travelsPerDay, daysLeft) {
+function createOptionsForNode(traversedEdges, totalTravelsCount, travelsPerDay, daysLeft) {
 
     if (daysLeft <= 0) {
         // We have found a solution
         let totalCostFull = 0
         let totalCostReduced = 0
-        traversedEdges.forEach(edge => {
+        let edgesWithoutRootNode = traversedEdges.slice(1, traversedEdges.length)
+        edgesWithoutRootNode.forEach(edge => {
             totalCostFull += edge.count * edge.ticket.priceFull
             totalCostReduced += edge.count * edge.ticket.priceReduced
         })
         return [{
-            edges: traversedEdges,
+            edges: edgesWithoutRootNode,
             totalCostFull: totalCostFull,
             totalCostReduced: totalCostReduced,
         }]
     }
 
     let options = []
-    let edgesToExplore = generateEdgesForNode(remainingTicketTypes, travelsPerDay, daysLeft)
-
+    let edgesToExplore = generateEdgesForNode(traversedEdges, travelsPerDay, daysLeft)
+    // Om man har tagit en väg men en mindre storlek som inte överflödar, då får man bara använda singleTickets
     edgesToExplore.forEach(edge => {
-        let filteredRemainingTickets = remainingTicketTypes.filter(ticket => ticket.days < edge.ticket.days)
         let newDaysLeft = daysLeft - edge.days
 
-        let newOptions = createOptionsForNode([...traversedEdges, edge], filteredRemainingTickets, totalTravelsCount, travelsPerDay, newDaysLeft)
+        let newOptions = createOptionsForNode([...traversedEdges, edge], totalTravelsCount, travelsPerDay, newDaysLeft)
         options.push(...newOptions)
     })
 
     return options
 }
 
-function generateEdgesForNode(remainingTicketTypes, travelsPerDay, daysLeft) {
+function generateEdgesForNode(traversedEdges, travelsPerDay, daysLeft) {
     let edges = []
-    remainingTicketTypes
+    let largestTicketThatFitsDaysLeft = null
+
+    let currentEdge = traversedEdges[traversedEdges.length - 1]
+    let remainingTickets = currentEdge.remainingTickets
+
+    remainingTickets
         .filter(ticket => ticket != singleTicket)
-        .filter(ticket => ticket.days > 3 || daysLeft < 10)
         .forEach(ticket => {
-            for (ticketCount = 1; ticketCount * ticket.days <= daysLeft; ticketCount++) {
+            if (largestTicketThatFitsDaysLeft == null
+                || (ticket.days > largestTicketThatFitsDaysLeft.days && ticket.days <= daysLeft)) {
+                largestTicketThatFitsDaysLeft = ticket
+            }
+        })
+
+
+    remainingTickets
+        .filter(ticket => ticket != singleTicket)
+        .forEach(ticket => {
+            let maxTicketCountWithNoOverflow = Math.floor(daysLeft / ticket.days)
+
+            if (maxTicketCountWithNoOverflow > 0) {
+                let days = ticket.days * maxTicketCountWithNoOverflow
+                let newRemainingTickets = null
+
+                if (ticket.days < largestTicketThatFitsDaysLeft.days) {
+                    newRemainingTickets = remainingTickets.filter(ticket => ticket == singleTicket)
+                } else {
+                    newRemainingTickets = remainingTickets.filter(ticket => ticket.days < days)
+                }
+
                 edges.push({
-                    "ticket": ticket,
-                    "count": ticketCount,
-                    "days": ticket.days * ticketCount
+                    ticket: ticket,
+                    count: maxTicketCountWithNoOverflow,
+                    days: days,
+                    remainingTickets: newRemainingTickets
                 })
             }
 
             // Add the last one that exceeds the required nr of days unless it's a 24h ticket
             if (ticket != dayTicket && daysLeft % ticket.days != 0) {
+                let days = ticket.days * (maxTicketCountWithNoOverflow + 1)
                 edges.push({
-                    "ticket": ticket,
-                    "count": ticketCount,
-                    "days": ticket.days * ticketCount
+                    ticket: ticket,
+                    count: maxTicketCountWithNoOverflow + 1,
+                    days: days,
+                    remainingTickets: remainingTickets.filter(ticket => ticket.days < days)
                 })
             }
         })
 
-    if (remainingTicketTypes.includes(singleTicket)) {
+    if (remainingTickets.includes(singleTicket)) {
         edges.push({
-            "ticket": singleTicket,
-            "count": Math.ceil(daysLeft * travelsPerDay),
-            "days": daysLeft
+            ticket: singleTicket,
+            count: Math.ceil(daysLeft * travelsPerDay),
+            days: daysLeft,
+            remainingTickets: []
         })
     }
 
